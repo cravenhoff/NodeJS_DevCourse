6d6388c7c672a07ad0fb0e547c94d8a684339339
Crystal Kewe <cravenhoff@Crystals-MacBook-Air.local>
2020-11-22 04:17:28 +1000

    Module Wrapper Function.

--- person.js
+++ person.js
@@ -1,3 +1,12 @@
+/* Module Wrapper Function
+function (exports, require, module, __filename, __dirname) {
+    // Imported module code is encompasses in here...
+}
+*/
+
+// Proving that the arguments of the module wrapper function are accessible in this file
+console.log(__dirname, __filename);
+
 class Person {
     constructor(name, age) {
         this.name = name;
