47ec8df56f1e17edd43f7ade2f0cbdf35be84fb2
Crystal Kewe <cravenhoff@Crystals-MacBook-Air.local>
2020-11-22 05:05:55 +1000

    writeFile function of fs module.

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -2,10 +2,19 @@ const fs = require("fs");
 const path = require("path");
 
 // Create folder
-fs.mkdir(path.join(__dirname, "/test"), {}, err => {
+// fs.mkdir(path.join(__dirname, "/test"), {}, err => {
+//     if(err) {
+//         throw err;
+//     } else {
+//         console.log("Folder created...");
+//     }
+// })
+
+// Create and write file
+fs.writeFile(path.join(__dirname, "/test", "hello.txt"), "Hello Node World!", err => {
     if(err) {
         throw err;
-    } else {
-        console.log("Folder created...");
     }
+
+    console.log("File created and written to...");
 })
