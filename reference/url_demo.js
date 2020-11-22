f11beeb9e92e71cb762a269264d22e40cc76845a
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-11-22 23:58:50 +1000

    Host name (root domain)
    Extracted the host name / root domain of the sample URL using the host() function / method.

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -6,3 +6,6 @@ const myUrl = new URL("http://mywebsite.com/hello.html?id=100&status=active");
 // Serialize URL
 console.log(myUrl.href);
 console.log(myUrl.toString());
+
+// Get host (root domain)
+console.log(myUrl.host);
