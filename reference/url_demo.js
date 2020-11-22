578a0539a34cad8abbc8f4c9af604aac022e9e42
Crystal Kewe <49733508+cravenhoff@users.noreply.github.com>
2020-11-22 23:57:59 +1000

    Serialize URL
    Serialized the sample URL.

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -2,3 +2,7 @@ const url = require("url");
 
 // Create sample URL
 const myUrl = new URL("http://mywebsite.com/hello.html?id=100&status=active");
+
+// Serialize URL
+console.log(myUrl.href);
+console.log(myUrl.toString());
