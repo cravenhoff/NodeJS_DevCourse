0ce73e6d0cefaeea7f40adbbc032f93cca9ceb6d
Crystal A. Kewe <crystalkewe@gmail.com>
2020-11-23 21:24:38 +1000

    Common URL Module Methods.

--- reference/url_demo.js
+++ reference/url_demo.js
@@ -1,9 +1,12 @@
 const url = require("url");
 
-// Create sample URL
-const myUrl = new URL("http://mywebsite.com/hello.html?id=100&status=active");
+// Instantiate new sample URL
+const myUrl = new URL("http://mywebsite.com:8000/hello.html?id=100&status=active");
 
-// Serialize URL
+// Output sample URL
+console.log(myUrl);
+
+// Serialized URL
 console.log(myUrl.href);
 console.log(myUrl.toString());
 
@@ -12,3 +15,12 @@ console.log(myUrl.host);
 
 // Get hostname
 console.log(myUrl.hostname);
+
+// Get pathname
+console.log(myUrl.pathname);
+
+// Serialized query
+console.log(myUrl.search);
+
+// Query (params) object
+console.log(myUrl.searchParams);
