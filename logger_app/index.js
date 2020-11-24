81ccf93aaac90979bff81cd271b36aecaca26896
Crystal A. Kewe <crystalkewe@gmail.com>
2020-11-24 22:19:18 +1000

    Used FS Module to add/append logs to file.

--- logger_app/index.js
+++ logger_app/index.js
@@ -1,10 +1,22 @@
 const Logger = require("./logger");
+const fs = require("fs");
+const path = require("path");
 
 // Instantiate new logger{} object
 const logger = new Logger();
 
 logger.on("message", (data) => {
-    console.log("Listener Called: ", data);
+    // Console out data
+    // console.log("Listener Called: ", data);
+
+    // Append data to logs.txt file and read from it
+    fs.writeFile(path.join(__dirname, "logs.txt"), `${data.id}: ${data.msg} \n`, err => {
+        if(err) {
+            throw err;
+        }
+
+        console.log("File successfully written to...");
+    })
 })
 
 logger.log("Hello There!");
--- logger_app/logger.js
+++ logger_app/logger.js
@@ -1,9 +1,12 @@
 const EventEmitter = require("events");
 const uuid = require("uuid");
+const fs = require("fs");
+const path = require("path");
 
 // console.log(uuid.v4());
 
 class Logger extends EventEmitter {
+    // log() Method
     log(msg) {
         // Call event
         this.emit("message", {id: uuid.v4(), msg});
