6b71ff4f65c01506f2134c945c332ef17a36ef6d
Crystal Kewe <cravenhoff@Crystals-MacBook-Air.local>
2020-11-26 05:29:49 +1000

    More yarg commands - list and read.

--- 2. Complete Node Developer Course - Andrew Mead/notes-app/app.js
+++ 2. Complete Node Developer Course - Andrew Mead/notes-app/app.js
@@ -23,5 +23,29 @@ yargs.command({
     }
 });
 
+/* --- Challenge: Add two new commands ---
+1. Setup command to support "list" command {print out placeholder message for now}
+2. Setup command to support "read" command {print out placeholder message for now}
+3. Test your wokr by running both commands and ensure correct output
+*/
+
+// Create "list" command
+yargs.command({
+    command: "list",
+    describe: "List all notes",
+    handler: function() {
+        console.log(chalk.bgBlue.black("Listing all the notes!"));
+    }
+});
+
+// Create "read" command
+yargs.command({
+    command: "read",
+    describe: "Read note",
+    handler: function() {
+        console.log(chalk.bgWhite.black("Reading the note!"));
+    }
+})
+
 // yargs parsing
 console.log(yargs.argv); 
