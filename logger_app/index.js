20d6f5e9593ef859d42b7b2a9c74ed312736461d
Crystal Kewe <cravenhoff@Crystals-MacBook-Air.local>
2020-11-25 17:32:38 +1000

    Added a few final closing messages to be logged.

--- logger_app/index.js
+++ logger_app/index.js
@@ -26,10 +26,14 @@ logger.on("message", (data) => {
 })
 
 // Messages for logx.txt file
-logger.log("Hello There!");
-logger.log("Hello You!");
-logger.log("Hello People!");
+// logger.log("Hello There!");
+// logger.log("Hello You!");
+// logger.log("Hello People!");
 logger.log("Adding more now that we got the file logging correct.");
 logger.log("Let's give it up some more for the hussle.");
 logger.log("Who said it couldn't be done, huh?!");
 logger.log("We just proved them wrong, again...");
+logger.log("We optimized the codebase and made it better and much more clearner!");
+logger.log("appendFil() and readFile() did the job perfectly.");
+logger.log("Let's see if we can build on from this to create even better applications...");
+logger.log("Peace... This is the last message from me... for now! :)");
