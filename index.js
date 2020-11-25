eb00762548f4985edc40c6019d24a2a0f3e54d10
Crystal Kewe <crystalkewe@gmail.com>
2020-11-25 19:12:41 +1000

    Added about.html page route to the server.

--- index.js
+++ index.js
@@ -7,9 +7,24 @@ const server = http.createServer((req, res) => {
     // Test request url
     console.log(req.url);
 
+    // index.html file and root url
     if(req.url === "/") {
         // Output contents of public/index.html file
         fs.readFile(path.join(__dirname, "public", "index.html"), (err, content) => {
+            if(err) {
+                throw err;
+            } else {
+                // Specify the status code and content type
+                res.writeHead(200, {"Content-Type":"text/html"});
+                res.end(content);
+            }
+        })
+    }
+
+    // about.html file
+    if(req.url === "/about") {
+        // Output contents of public/index.html file
+        fs.readFile(path.join(__dirname, "public", "about.html"), (err, content) => {
             if(err) {
                 throw err;
             } else {
