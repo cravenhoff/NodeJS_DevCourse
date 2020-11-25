d49c78998e06b787e62a3ab32db6fcec116b14f8
Crystal Kewe <cravenhoff@Crystals-MacBook-Air.local>
2020-11-26 05:06:21 +1000

    Parsing CL arguments with yargs npm library.

--- 2. Complete Node Developer Course - Andrew Mead/notes-app/app.js
+++ 2. Complete Node Developer Course - Andrew Mead/notes-app/app.js
@@ -1,10 +1,12 @@
 const chalk = require("chalk"); // chalk npm library
+const yargs = require("yargs");
 const getNotes = require("./notes.js"); // Include or import notes.js
 
 // -- Command line arguments
 const args = process.argv; // Holds any arguments passed to the terminal
+console.log(chalk.bgWhite.red("--- Unparsed ---"));
 console.log(args); // args[0] => node exectutable and args[1] => javascript filepath
-console.log(chalk.inverse(args[2]));
+// console.log(chalk.inverse(args[2])); // Raw arguments, no parsing
 
 // CL third argument
 const command = process.argv[2];
@@ -14,3 +16,6 @@ if(command === "add") {
 } else if(command === "remove") {
     console.log(chalk.bgRed.black("Removing note!"));
 }
+
+console.log(chalk.bgWhiteBright.green("--- Parsed ---"));
+console.log(yargs.argv); // Parse command line arguments
