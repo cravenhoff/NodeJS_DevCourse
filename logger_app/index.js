a3a2e2472ea1c0e504b0b850ec1f4e98f807434c
Crystal Kewe <cravenhoff@Crystals-MacBook-Air.local>
2020-11-25 17:20:18 +1000

    Messages logged properly in logs.txt file.

--- logger_app/index.js
+++ logger_app/index.js
@@ -5,18 +5,13 @@ const path = require("path");
 // Instantiate new logger{} object
 const logger = new Logger();
 
+
 logger.on("message", (data) => {
     // Console out data
     // console.log("Listener Called: ", data);
 
-    // Write data to logs.txt file
-    fs.writeFile(path.join(__dirname, "logs.txt"), `${data.id}: ${data.msg} \n`, err => {
-        if(err) {
-            throw err;
-        }
-
-        console.log("File successfully written to...");
-
+    // Check if logs.txt file exists
+    if(path.join(__dirname, "logs.txt")) {
         // Append data to logs.txt file
         fs.appendFile(path.join(__dirname, "logs.txt"), `${data.id}: ${data.msg} \n`, err => {
             if(err) {
@@ -34,9 +29,40 @@ logger.on("message", (data) => {
 
             console.log(data);
         })
-    })
+    } else {
+        // Write data to logs.txt file
+        fs.writeFile(path.join(__dirname, "logs.txt"), "logs.txt file created...\n"/*`${data.id}: ${data.msg} \n`*/, err => {
+            if(err) {
+                throw err;
+            }
+
+            console.log("File successfully written to...");
+
+            // Append data to logs.txt file
+            fs.appendFile(path.join(__dirname, "logs.txt"), `${data.id}: ${data.msg} \n`, err => {
+                if(err) {
+                    throw error;
+                } else {
+                    console.log("File successfully appended...");
+                }
+            })
+
+            // Read from logs.txt file
+            fs.readFile(path.join(__dirname, "logs.txt"), "utf8", (err, data) => {
+                if(err) {
+                    throw err;
+                }
+
+                console.log(data);
+            })
+        })
+    }
 })
 
 logger.log("Hello There!");
 logger.log("Hello You!");
 logger.log("Hello People!");
+logger.log("Adding more now that we got the file logging correct.");
+logger.log("Let's give it up some more for the hussle.");
+logger.log("Who said it couldn't be done, huh?!");
+logger.log("We just proved them wrong, again...");
