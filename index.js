dddcb2178808e9ede0620e00bf23091d5eb1a519
Crystal Kewe <crystalkewe@gmail.com>
2020-11-25 18:54:58 +1000

    Created simple server @ port:5000 to display when req.url === home or index file.

---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const path = require("path");
+const fs = require("fs");
+
+// Create new server
+const server = http.createServer((req, res) => {
+    // Test request url
+    console.log(req.url);
+
+    if(req.url === "/") {
+        res.end("<h1>Home</h1>");
+    }
+});
+
+// Port listener
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
