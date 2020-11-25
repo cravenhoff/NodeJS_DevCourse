892aa381759d896c1bc494b8f49df2fedef809a7
Crystal Kewe <crystalkewe@gmail.com>
2020-11-25 19:17:08 +1000

    Sample Rest API or Microservice in Node.

--- index.js
+++ index.js
@@ -34,6 +34,17 @@ const server = http.createServer((req, res) => {
             }
         })
     }
+
+    // Rest API or Microserver example
+    if(req.url === "/api/users") {
+        const users = [
+            {name: "Bob Smith", age: 40},
+            {name: "John Doe", age: 30}
+        ];
+
+        res.writeHead(200, {"Content-Type":"application/json"});
+        res.end(JSON.stringify(users));
+    }
 });
 
 // Port listener
