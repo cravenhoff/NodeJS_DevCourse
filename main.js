190f5751df06988057ca92c3e151f80dcc478a68
Crystal Kewe <crystalkewe@gmail.com>
2020-11-26 00:05:00 +1000

    Committing deleted files that were moved.

--- main.js
+++
@@ -1,9 +0,0 @@
-const Person = require("./person.js");
-
-const person1 = new Person("John Doe", 30);
-
-console.log("Hello from Node.js...");
-console.log(person1);
-console.log(person1.name);
-console.log(person1.age);
-person1.greeting();
