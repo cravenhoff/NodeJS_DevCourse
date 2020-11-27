ae6dcd243712675ebbaf867f7410cb78a16a5a0b
Crystal Kewe <cravenhoff@Crystals-MacBook-Air.local>
2020-11-28 00:32:00 +1000

    Added second Remove-a-Note Challenge Instructions.

--- 2. Complete Node Developer Course - Andrew Mead/notes-app/app.js
+++ 2. Complete Node Developer Course - Andrew Mead/notes-app/app.js
@@ -33,12 +33,25 @@ yargs.command({
 3. Call removeNote in remove command handler
 4. Have removeNote log the title of the note to be removed
 5. Test your work using: node app.js remove --title="some title"
+
+--- Challenge: Wire up removeNote ---
+1. Load existing notes
+2. Use array filter method to remove the matching note (if any)
+3. Save the newly created array
+4. Test your work with a title that exists and a title that doesn't exist
 */
 
 // Create "remove" command
 yargs.command({
     command: "remove",
     describe: "Remove a note",
+    builder: {
+        title: {
+            describe: "Note title",
+            demandOption: true,
+            type: "string"
+        }
+    },
     handler: function(argv) {
         console.log(chalk.bgRed.black("Removing note..."));
         notes.removeNote(argv.title);
