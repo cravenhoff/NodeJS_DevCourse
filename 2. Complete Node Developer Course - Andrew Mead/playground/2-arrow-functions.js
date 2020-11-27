5c2d21573faf4eabf3d2f5fdb021ed5585c32b6b
Crystal Kewe <cravenhoff@Crystals-MacBook-Air.local>
2020-11-28 02:14:53 +1000

    Non-binding 'this' keyword using arrow functions.

--- 2. Complete Node Developer Course - Andrew Mead/playground/2-arrow-functions.js
+++ 2. Complete Node Developer Course - Andrew Mead/playground/2-arrow-functions.js
@@ -15,6 +15,11 @@ const event = {
     guestList: ["Andrew", "Jen", "Mike", "Shawn"],
     printGuestList: function() {
         console.log("Guest list for " + this.name);
+
+        // Arrow functions are poor options for method functions or object properties
+        this.guestList.forEach((guest) => { // Do not bind their own value but that of their context or parent scope.
+            console.log(guest + " is attending the " + this.name);
+        })
     }
 }
 
