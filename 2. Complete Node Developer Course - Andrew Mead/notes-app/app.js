d5cbcb285d7afbc9bcb92b88b33bd7749b8c646e
cravenhoff <crystalkewe@gmail.com>
2020-11-28 03:04:47 +1000

    New challenge: refactor all functions in notes app.

--- 2. Complete Node Developer Course - Andrew Mead/notes-app/app.js
+++ 2. Complete Node Developer Course - Andrew Mead/notes-app/app.js
@@ -2,6 +2,13 @@ const chalk = require("chalk");
 const yargs = require("yargs");
 const notes = require("./notes.js");
 
+
+/* --- Goal: Refactor all functions ---
+1. If function is a method, use ES6 method definition syntax
+2. Otherwise, use most concise arrow function possible
+3. Test your work
+*/
+
 // Customize yargs version
 yargs.version("1.1.0");
 
