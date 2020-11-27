a91b6ce0996746a2675cd7cb003f7ca6cb580e28
Crystal Kewe <cravenhoff@Crystals-MacBook-Air.local>
2020-11-27 17:18:33 +1000

    Stringify and parsing JSON Data.

---
+++ 2. Complete Node Developer Course - Andrew Mead/playground/1-json.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+
+// Create sample object
+const book = {
+    title: "Grit",
+    author: "Angela Duckworth"
+}
+
+// Output book{} object
+console.log(book);
+
+// Stringify the object to JSON
+const JSONData = JSON.stringify(book);
+// Output JSON Data
+console.log(JSONData);
+console.log(JSONData.title); // Can't access property as JSONData is now a string.
+
+// Parse JSON Data
+const parsedData = JSON.parse(JSONData);
+console.log(parsedData.author);
+    
