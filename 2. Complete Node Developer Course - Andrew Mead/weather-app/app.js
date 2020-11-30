d2b83a8f745a5b513e4f614940bc26e6f8513d18
cravenhoff <crystalkewe@gmail.com>
2020-12-01 00:48:24 +1000

    Updated the units for the temperature and feelslike properties  from metrics to fahrenheit.

--- 2. Complete Node Developer Course - Andrew Mead/weather-app/app.js
+++ 2. Complete Node Developer Course - Andrew Mead/weather-app/app.js
@@ -1,7 +1,7 @@
 const request = require("request");
 
 // Store the weather API url
-const url = "http://api.weatherstack.com/current?access_key=ce0de027b68728e11dc342f845708fac&query=37.8267,-122.4233";
+const url = "http://api.weatherstack.com/current?access_key=ce0de027b68728e11dc342f845708fac&query=37.8267,-122.4233?units=f";
 
 /* --- Goal: Print a small forecast to the user ---
 1. Print: "It is currently 9 degress out. It feels like 5 degrees out."
@@ -11,5 +11,5 @@ const url = "http://api.weatherstack.com/current?access_key=ce0de027b68728e11dc3
 // Use request module
 request({url, json: true}, (err, response) => {
     // console.log(response.body.current);
-    console.log(`It is currently ${response.body.current.temperature} degrees out. It feels like ${response.body.current.feelslike} degrees out.`);
+    console.log("It is currently " + response.body.current.temperature + " degrees out. It feels like " + response.body.current.feelslike + " degrees out.");
 });
