4bf5bb8b569a0df323ab975ecb6a5f14798b996d
cravenhoff <crystalkewe@gmail.com>
2021-01-04 22:46:02 +1000

    Playing around with asynchronous functions using setTimeeOut().

--- 2. Complete Node Developer Course - Andrew Mead/playground/0-asynch.js
+++ 2. Complete Node Developer Course - Andrew Mead/playground/0-asynch.js
@@ -1,8 +1,14 @@
 // Understanding Asynchronous Node
 console.log("Starting");
 
+// Runs after two seconds
 setTimeout(() => {
     console.log("2 Second Timer");
-}, 2000)
+}, 2000);
+
+// Runs immediately
+setTimeout(() => {
+    console.log("0 Second Timer");
+}, 0);
 
 console.log("Stopping");
